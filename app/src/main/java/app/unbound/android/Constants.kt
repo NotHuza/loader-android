2eda1ebd7b31e6b1a9f04d84a772c4bea96e866f
Magich <37085494+NotHuza@users.noreply.github.com>
2025-01-26 14:04:27 +0100

    remove test

--- app/src/main/java/app/unbound/android/Constants.kt
+++ app/src/main/java/app/unbound/android/Constants.kt
@@ -1,42 +1,38 @@
 package app.unbound.android
 
-import android.content.Context
-import android.util.Log
 import com.google.gson.JsonElement
 import com.google.gson.annotations.SerializedName
-import java.io.BufferedReader
-import java.io.InputStreamReader
 
-data class Addon(
+data class Addon (
     val bundle: Any,
     val manifest: Manifest
 )
 
-data class Theme(
+data class Theme (
     val bundle: ThemeJSON,
     val manifest: Manifest
 )
 
-data class ThemeJSON(
+data class ThemeJSON (
     @SerializedName("raw") val raw: JsonElement?,
     @SerializedName("semantic") val semantic: JsonElement?,
     @SerializedName("background") val background: JsonElement?
 )
 
-data class Manifest(
-    @SerializedName("name") var name: String,
-    @SerializedName("id") var id: String,
-    @SerializedName("description") var description: String,
-    @SerializedName("version") var version: String,
-    @SerializedName("authors") var authors: ArrayList<Authors>,
-    @SerializedName("bundle") var bundle: String
+data class Manifest (
+    @SerializedName("name") var name : String,
+    @SerializedName("id") var id : String,
+    @SerializedName("description") var description : String,
+    @SerializedName("version") var version : String,
+    @SerializedName("authors") var authors : ArrayList<Authors>,
+
+    @SerializedName("bundle") var bundle : String
 )
 
-data class Authors(
-    @SerializedName("name") var name: String,
-    @SerializedName("id") var id: String
+data class Authors (
+    @SerializedName("name") var name : String,
+    @SerializedName("id") var id : String
 )
-
 
 class Constants {
     companion object {
@@ -50,30 +46,5 @@ class Constants {
         const val FILE_LOAD = "LoadScriptFromFile"
         const val ASSET_LOAD = "LoadScriptFromAssets"
         const val NEW_ACTIVITY = "newActivity"
-
-        
-         
-        fun loadAsset(context: Context, fileName: String): String {
-            return try {
-                
-                val assetManager = context.assets
-                val inputStream = assetManager.open(fileName)
-                val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-                val stringBuilder = StringBuilder()
-                var line: String?
-
-                
-                while (bufferedReader.readLine().also { line = it } != null) {
-                    stringBuilder.append(line).append("\n")
-                }
-
-                bufferedReader.close()
-                stringBuilder.toString() 
-            } catch (e: Exception) {
-                
-                Log.e("AssetLoader", "Error loading asset '$fileName': ${e.message}")
-                "Error loading asset '$fileName': ${e.message}"
-            }
-        }
     }
 }
